41d05b1e825394a8c08ba56a8355b30c32456d6d
Anson Chan <ananryry180@gmail.com>
2026-08-04 23:24:51 -0700

    squeezed navbar together

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const links = [
+  { label: "about", href: "/about" },
   { label: "work", href: "/work" },
   { label: "projects", href: "/projects" }
 ];
