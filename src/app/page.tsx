61a45967f98c7b97c582361ba944e1cb5eb2c3f2
Anson Chan <ananryry180@gmail.com>
2026-09-22 00:19:11 -0700

    change connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,8 +101,12 @@ export default function Home() {
             <p className="v2-flags" aria-label="Hong Kong and Australia">🇭🇰 🇦🇺</p>
             <p className="v2-intro-about">
               Hi! I&apos;m Anson. I was born in <span className="v2-intro-highlight">Hong Kong</span> and
-              raised in <span className="v2-intro-highlight">Australia</span> for 15 years before moving to <span className="v2-intro-highlight">Vancouver, Canada</span> in 2021. I enjoy building software people use and hope to help
-              everyday life become a little easier and a little better. I&apos;m currently studying
+              raised in <span className="v2-intro-highlight">Australia</span> for 15 years before moving to <span className="v2-intro-highlight">Vancouver, Canada</span> in 2021. I enjoy building software to make daily life
+              easier and better, even in just a small way.
+
+              <br></br>
+              <br></br>
+              I&apos;m currently studying
               <span className="v2-intro-highlight"> Computer Engineering</span> at the <span className="v2-intro-highlight">University of British Columbia</span> and interning
                at <span className="v2-intro-highlight">ScalePad</span>.
 
@@ -171,7 +175,7 @@ export default function Home() {
           </div>
           <div className="v2-connect-card v2-card">
             <p className="v2-seeking">Seeking Summer 2027 internships.</p>
-            <p>Have something in mind? I&apos;d love to hear about it.</p>
+            <p>Have something in mind? Whether it’s an internship opportunity, a coffee chat, or an overly competitive game of pickleball, I’d love to hear from you! </p>
             <ContactLinks label="Connect with Anson" />
           </div>
         </section>
